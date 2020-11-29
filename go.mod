5dc2cc29386e82ef05a1563daf313744f3616ab6
chengchaos <chengchaos@gmail.com>
2020-11-29 21:29:57 +0800

    改一下httprouter 的包名称看看效果

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/chengchaos/go-web-demo
 
 go 1.14
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require github.com/chengchaos/httprouter v1.2.0
--- l3-main/main.go
+++ l3-main/main.go
@@ -2,22 +2,30 @@ package main
 
 import (
 	"fmt"
-	"github.com/julienschmidt/httprouter"
 	"net/http"
+
+	"github.com/chengchaos/httprouter"
 )
 
 func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	fmt.Fprintf(w, "hello %s\n", p.ByName("name"))
 }
 
+func root(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprint(w, "<h1>It works!</h1><br /><a href='/hello/chengchao'>chengchao</a>")
+}
+
 func main() {
 	mux := httprouter.New()
 	mux.GET("/hello/:name", hello)
+	mux.GET("/", root)
 
-	server := http.Server {
-		Addr : "127.0.0.1:8080",
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
 		Handler: mux,
 	}
 
+	fmt.Println("server is startting ...")
 	server.ListenAndServe()
+	fmt.Println("server is stoped!")
 }
